5b834a5a0a1e670bfc348cced3c821b6b6a79375
basecamp <cvanderpoel@basecampcodingacademy.org>
2018-07-05 10:51:53 -0500

    Just making sure that this is saved

--- chore_assigner.py
+++ chore_assigner.py
@@ -1,4 +1,4 @@
-from random import choice
+from random import shuffle
 
 
 def chore_assigner():
@@ -7,8 +7,6 @@ def chore_assigner():
         'Henry', 'John', 'Irma', 'Danny', 'Jakylan', 'Justice', 'Ray', 'Cody',
         'Andrew'
     ]
-    print(students)
-
     chores = [
         'Monday Lunch', 'Tuesday Lunch', 'Wednesday Lunch', 'Thursday Lunch',
         'Friday Lunch', 'Friday Kitchen Clean', 'Friday Bathroom Small Clean ',
@@ -18,18 +16,9 @@ def chore_assigner():
         'Friday Hallway Sweep', 'Friday Stairway Sweep',
         'Daily Inspections and Friday Wipe Down'
     ]
-    print(chores)
-
-    while True:
-        assigned_chore = []
-        stdnt = []
-        for student in students:
-            stdnt.append(student)
-        chr = []
-        for chore in chores:
-            chr.append(chore)
-        assigned_chore.extend([stdnt.pop(), chr.pop()])
-        print(assigned_chore)
+    shuffle(chores)
+    for student in students:
+        print(student, ':', chores.pop())
 
 
 def main():
